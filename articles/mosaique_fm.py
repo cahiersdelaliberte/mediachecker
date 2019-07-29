2b1d36cac897b608076a8b3f7b73b04f9f11a4c8
sandcha <sandcha@users.noreply.github.com>
2019-07-29 22:37:00 +0100

    Uniformisation des colonnes de CSV pour NewsAPI et MosaïqueFM
    newsapi title = mosaique Title
    newsapi publishedAt = mosaique Date
    newsapi url = mosaique url
    newsapi content = mosaique Article
    
    Avant :
    tunis.csv : source,author,title,description,url,urlToImage,publishedAt,content
    newsapi : ['title', 'description', 'publishedAt', 'source', 'author', 'urlToImage']
    mosaique : url,Section,Titre,Date,Article

--- articles/mosaique_fm.py
+++ articles/mosaique_fm.py
@@ -23,38 +23,45 @@ OUTPUT_PATH = './mosaique.csv'
 
 
 #récupérer les urls des articles de la rubrique Politique
-artical_link=[]  
-url_info = "/fr/actualite-politique-tunisie/" 
-url = "https://www.mosaiquefm.net/fr/actualites/actualite-politique-tunisie/4/"
-#pour cet exemple, on scrape uniquement les 4 premières pages de recherche
-nb_pages = 4
-for num_page in range(1, nb_pages + 1):
-    soup = BeautifulSoup(urllib.request.urlopen(url+str(num_page)), "html.parser") #on récupère le code source de chaque url (page de recherche)
-    for a in soup.find_all('a', href=True): 
-        #on cherche dans toutes les balise <a> les "href" dont l'url commence par /r/
-        if url_info in a['href']:
-            artical_link.append("https://www.mosaiquefm.net"+a['href'])
+def scrap_political_articles_list():
+    article_link=[]  
+    url_info = "/fr/actualite-politique-tunisie/" 
+    url = "https://www.mosaiquefm.net/fr/actualites/actualite-politique-tunisie/4/"
+    #pour cet exemple, on scrape uniquement les 4 premières pages de recherche
+    nb_pages = 4
+    for num_page in range(1, nb_pages + 1):
+        soup = BeautifulSoup(urllib.request.urlopen(url+str(num_page)), "html.parser") #on récupère le code source de chaque url (page de recherche)
+        for a in soup.find_all('a', href=True): 
+            #on cherche dans toutes les balise <a> les "href" dont l'url commence par /r/
+            if url_info in a['href']:
+                article_link.append("https://www.mosaiquefm.net"+a['href'])
+    return article_link
 
 
-title    = []
-article  = []
-key_word = []
-date_article =  []
-for link in artical_link:
-    page = requests.get(link) #ouvrir l'url du CV
-    tree = lxml_html.fromstring(page.content)
-    key_word.append('Politique')
-    part1 = tree.xpath('//div/p/strong/text()')  #ça fonctionne
-    part2 = tree.xpath('//div[@class="desc descp__content"]/p/text()') #ça fonctionne
-    article.append(' '.join(part1+part2))
-    date_article.append(tree.xpath('//div[@class="date text-center"]/text()')[0].strip()) #ça fonctionne
-    title.append(tree.xpath('//div/h1[@class="title"]/text()')[0].strip())
+def scrap_articles_information(articles_links):
+    title    = []
+    article  = []
+    key_word = []
+    article_date =  []
+    for link in articles_links:
+        page = requests.get(link) #ouvrir l'url du CV
+        tree = lxml_html.fromstring(page.content)
+        key_word.append('Politique')
+        part1 = tree.xpath('//div/p/strong/text()')  #ça fonctionne
+        part2 = tree.xpath('//div[@class="desc descp__content"]/p/text()') #ça fonctionne
+        article.append(' '.join(part1+part2))
+        article_date.append(tree.xpath('//div[@class="date text-center"]/text()')[0].strip()) #ça fonctionne
+        title.append(tree.xpath('//div/h1[@class="title"]/text()')[0].strip())
+
+    #Consolider les données scrapées dans un dataframe 
+    #X=np.column_stack((article_link,key_word,title,article_date,article)) 
+    X=np.column_stack((title, article_date, articles_links, article))
+    df=pd.DataFrame(X)
+    # df.columns = ['url','Section','Titre','Date','Article']
+    ## df.columns = ['Titre', 'Date', 'url', 'Article']
+    df.columns = ['titre', 'date', 'url', 'article']
+    return df
 
 
-#Consolider les données scrapées dans un dataframe 
-X=np.column_stack((artical_link,key_word,title,date_article,article)) 
-df=pd.DataFrame(X)
-df.columns = ['url','Section','Titre','Date','Article']
-#print(df)
-
-df.to_csv(OUTPUT_PATH, index=False)
+articles_dataframe = scrap_articles_information(scrap_political_articles_list())
+articles_dataframe.to_csv(OUTPUT_PATH, index=False)
--- clustering/clustering_news.py
+++ clustering/clustering_news.py
@@ -72,76 +72,76 @@ def generateWC(wc, name, dico, nbclusters, y_pred):
          plot_num += 1
         
          if plot_num > nbclusters :
-             break
+            break
     
     plt.savefig('out-wc_' + name + '.png') #, bbox_inches='tight', dpi = 900, transparent = True)
-#    plt.show()
+    # plt.show()
     plt.close()
-    
+
+
 def newsjsontocsv(inpath, outpath):
     header = []
     with open(inpath, 'r') as data_file:
         jsonarticles = json.load(data_file)
         with open(outpath, 'w') as csvfile:
             w = csv.writer(csvfile, lineterminator='\n')
-#            w.writerow(rawIndex)
-            for article in jsonarticles['articles'] :
-                for elem in article :
-                    header.append(elem)
-                break
+            # w.writerow(rawIndex)
+            header = ['titre', 'date', 'url', 'article']    
             w.writerow(header)   
             for article in jsonarticles['articles'] :
                 try :
                     rawIndex = []
-                    article['source'] = article['source']['name']
-                    article['description'] = article['description'].replace("[Tunis Afrique Presse]", '')
-                    for elem in article :
-                        rawIndex.append(strip_accents(article[elem]).encode('utf-8'))
+                    # article['source'] = article['source']['name']
+                    # article['description'] = article['description'].replace("[Tunis Afrique Presse]", '')
+                    rawIndex.append(strip_accents(article['title']).encode('utf-8'))
+                    rawIndex.append(strip_accents(article['publishedAt']).encode('utf-8'))
+                    rawIndex.append(strip_accents(article['url']).encode('utf-8'))
+                    rawIndex.append(strip_accents(article['content']).encode('utf-8'))
                             
                     w.writerow(rawIndex)
                 except :
                     pass
-#                    print(article['title'])
+                    # print(article['title'])
 
 
 def datacsv(outpath, nbclusters=9):
     data = pd.read_csv(outpath)
-    df = pd.DataFrame(columns = data.columns)
-    df = data[['source', 'author', 'title', 'description', 'url', 'urlToImage', 'publishedAt']]
+    # df = pd.DataFrame(columns = ['titre', 'date', 'url', 'article'])
+    df = data[['titre', 'date', 'url', 'article']]
     
     vect = TfidfVectorizer(stop_words=tabSW, max_df=0.90, min_df=1)#, ngram_range=(1, 2))
-#        vect = TfidfVectorizer(tokenizer=tokenize, stop_words='french', max_df=0.50, min_df=2)
+    # vect = TfidfVectorizer(tokenizer=tokenize, stop_words='french', max_df=0.50, min_df=2)
 
-#    X = vect.fit_transform(data["title"].astype('U'))
-#    X = vect.fit_transform(data["description"].astype('U'))
-    X = vect.fit_transform(data["description"] + ' ' + data["title"])
-#    X = StandardScaler(with_mean=False).fit_transform(X)
+    X = vect.fit_transform(data["titre"].astype('U'))
+    #  X = vect.fit_transform(data["description"].astype('U'))
+    ## X = vect.fit_transform(data["description"] + ' ' + data["title"])
+    #  X = StandardScaler(with_mean=False).fit_transform(X)
     
-    #vocabulary
+    # vocabulary
     feature_names = vect.get_feature_names()  
     vocab = open(OUTPUT_VOCABULARY, "w")
     for i, name in enumerate(feature_names):
         vocab.write(str(i) + '\t' + name + '\n')
     vocab.close() 
     
-#    for i, elem in enumerate(X):
-#        print ('\n', i, data["title"][i])
-#        for wordline in str(X[i]).split('\n') :
-#             index = wordline.split('\t')[0].split()[-1].replace(')','')
-#             print(feature_names[int(index)], '-', wordline.split('\t')[1])
+    #    for i, elem in enumerate(X):
+    #        print ('\n', i, data["title"][i])
+    #        for wordline in str(X[i]).split('\n') :
+    #             index = wordline.split('\t')[0].split()[-1].replace(')','')
+    #             print(feature_names[int(index)], '-', wordline.split('\t')[1])
     
     X = X.toarray()
     n_samples, n_features = X.shape
-    print ("n_samples:", n_samples, "n_features:", n_features)
+    print("n_samples:", n_samples, "n_features:", n_features)
     
     algorithm = cluster.SpectralClustering(n_clusters=nbclusters,
                                       eigen_solver='arpack',
                                       affinity="nearest_neighbors",
                                       assign_labels='discretize',
                                       random_state=73)
-#    algorithm = cluster.DBSCAN(eps=.2)
-#    algorithm = cluster.DBSCAN(eps=0.3)
-#    algorithm = cluster.AgglomerativeClustering(n_clusters=nbclusters, affinity='euclidean')
+    #  algorithm = cluster.DBSCAN(eps=.2)
+    #  algorithm = cluster.DBSCAN(eps=0.3)
+    #  algorithm = cluster.AgglomerativeClustering(n_clusters=nbclusters, affinity='euclidean')
     algorithm.fit(X)
     analyse(nbclusters, algorithm, X, data)
      
@@ -156,16 +156,16 @@ def analyse(nbclusters, algorithm, X, data) :
     dicoContent, dicoTitle = {}, {}
     dicoSources, dicoAuthors = {}, {}
     for i, value in enumerate(y_pred):
-#            print (i, value, data["created_at"][i], Counter(y_pred)[value])            
-            try : dicoContent[value] += " " + data["description"][i]
-            except : dicoContent[value] = data["description"][i]
+            # print (i, value, data["created_at"][i], Counter(y_pred)[value])            
+            try : dicoContent[value] += " " + data["article"][i]
+            except : dicoContent[value] = data["article"][i]
                 
-            try : dicoTitle[value] += " " + data["title"][i]
-            except : dicoTitle[value] = data["title"][i]
+            try : dicoTitle[value] += " " + data["titre"][i]
+            except : dicoTitle[value] = data["titre"][i]
             
-            author = deletion(["b\'","\'"], '', data["author"][i])
-            try : dicoAuthors[value] += " " + author
-            except : dicoAuthors[value] = author
+            # author = deletion(["b\'","\'"], '', data["author"][i])
+            # try : dicoAuthors[value] += " " + author
+            # except : dicoAuthors[value] = author
              
             deltab = [".", "/", ":", "-"]
             url = deletion(deltab, '', data["url"][i])
@@ -175,23 +175,24 @@ def analyse(nbclusters, algorithm, X, data) :
     # "./out-ArticlesClusters_"+str(nbclusters)+".csv"        
     with open(OUTPUT_ARTICLE_CLUSTERS, 'w') as csvfileWriter:
         w = csv.writer(csvfileWriter, lineterminator='\n')
-        header = ['source', 'author', 'title', 'description', 'url', 'urlToImage', 'publishedAt']
-#        header = ["created_at", "body", "type", "cluster"]
+        ## header = ['source', 'author', 'title', 'description', 'url', 'urlToImage', 'publishedAt']
+        # header = ["created_at", "body", "type", "cluster"]
+        header = ['titre', 'date', 'url', 'article']
         w.writerow(header)
         for i, value in enumerate(y_pred):
-            raw = [value, data["source"][i], data["title"][i], data["description"][i], data["url"][i], data["publishedAt"][i]]
+            raw = [value, data["titre"][i], data["date"][i], data["url"][i]]
             w.writerow(raw)
                 
-#    for l in sorted(dicoContent) :        
-#        print(l, '---', dicoFrom[l], dicoContent[l])
+    #    for l in sorted(dicoContent) :        
+    #        print(l, '---', dicoFrom[l], dicoContent[l])
 
     print (Counter(y_pred))
     
     #################################
 
     wc = WordCloud(mode="RGB", stopwords=tabSW, background_color=None, max_font_size=40, max_words=100, relative_scaling=0.5)
-#    generateWC(wc, "Sources_"+str(nbclusters), dicoSources, nbclusters, y_pred)
-#    generateWC(wc, "Authors_"+str(nbclusters), dicoAuthors, nbclusters, y_pred)
+    # generateWC(wc, "Sources_"+str(nbclusters), dicoSources, nbclusters, y_pred)
+    # generateWC(wc, "Authors_"+str(nbclusters), dicoAuthors, nbclusters, y_pred)
     generateWC(wc, "Title_"+str(nbclusters), dicoTitle, nbclusters, y_pred)
     generateWC(wc, "Content_"+str(nbclusters), dicoContent, nbclusters, y_pred)
     
